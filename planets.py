f73e512a11cd15042c93f858ec62cfeb45de810d
Mabel Shah <mshah14@unix2.csc.calpoly.edu>
2018-09-23 21:13:32 -0700

    finished lab

--- planets.py
+++ planets.py
@@ -1,6 +1,7 @@
 def weight_on_planets():
-   # write your code here
-   
+	earth = int(input("What do you weigh on earth? "))
+	print('\nOn Mars you would weigh %5.2f pounds.' %(earth * 0.38))
+	print('On Jupiter you would weigh %5.2f pounds.' %(earth * 2.34))	   
    
    
 if __name__ == '__main__':
